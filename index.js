e9ece8d728d704e7cbf67853de2dd773c61b6587
Kylee Sawyer <k.sawyer@ufl.edu>
2024-02-03 20:10:42 -0500

    added to index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+var questionsArr = 
+[
+    {
+        question: "Cats can jump more than 3 meters high.",
+        answer: true
+    },
+    
+    {
+        question: "Cats are actually afraid of mice.",
+        answer: false
+    },
+
+    {
+        question: "Cats can spend up to 14 hours asleep.",
+        answer: true
+    },
+
+    {
+        question: "A cat's tail helps them maintain better balance.",
+        answer: false
+    },
+
+    {
+        question: "Cats sweat through their paws.",
+        answer: true 
+    },
+
+    {
+        question: "Seafood is not recommended for cats.",
+        answer: true
+    }
+];
+
+function runQuiz() {
+    var numOfCorrects = 0
+    for (var i = 0; i <questionsArr.length; i++) {
+        var question = questionsArr[i].question
+        var answer = confirm(question)
+        if (answer === questionsArr[i].answer) {
+            numOfCorrects++
+        }
+    }
+    alert('Score: ' + Math.round((numOfCorrects/questionsArr.length)*100) + '%')
+}
